59e9c258e480482ce09fcaa40a961740605d0741
Felix Nagel <info@felixnagel.com>
2009-08-20 03:55:27 +0200

    delete pdfs

--- Formular/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// checking correkt data transfer
-	if (!empty($_POST['inputtext']) || !empty($_POST['txtarea']) || !empty($_POST['checkboxset']) || !empty($_POST['confirm'])) {
-		echo "true";
-	} else {
-		echo "Es ist ein technischer Fehler aufgetreten";
-	}
-	print_r($_POST);
-?>
