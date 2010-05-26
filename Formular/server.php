4927a03bccb815189a459a9eaf72b8ae15371f06
Felix Nagel <info@felixnagel.com>
2010-05-26 23:26:24 +0200

    initial: test branch for implementing jQuery support for lightbox

--- Formular/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-	// checking correkt data transfer
-	if ($_POST != "") {
-		echo "true";
-	} else {	
-		echo "There is a technical problem while receiving your data!";
-	}
-	// print_r($_POST);
-?>
