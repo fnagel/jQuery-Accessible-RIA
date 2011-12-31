cfa066a9d7ab51b486c88e97094253b8ea2115d7
Felix Nagel <info@felixnagel.com>
2011-12-31 14:19:27 +0100

    added: timestamp and minified versions

--- Formular/js/ui.formValidator.js
+++ Formular/js/ui.formValidator.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI FormValidator (24.12.10)
+ * jQuery UI FormValidator (10.10.11)
  * http://github.com/fnagel/jQuery-Accessible-RIA
  *
  * Copyright (c) 2009 Felix Nagel for Namics (Deustchland) GmbH
--- Tabs/js/ui.ariaTabs.js
+++ Tabs/js/ui.ariaTabs.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery UI AriaTabs (31.01.11)
+ * jQuery UI AriaTabs (31.12.11)
  * http://github.com/fnagel/jQuery-Accessible-RIA
  *
  * Copyright (c) 2009 Felix Nagel for Namics (Deustchland) GmbH
